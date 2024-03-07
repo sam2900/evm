a7e96edbf4d743f4849a73952d4229039aa74e2d
Saksham Mathur <144251472+sam2900@users.noreply.github.com>
2024-03-07 09:13:07 +0530

    univibe update

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className="border-t">
       <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
         <Link href='/'>
-          <Image 
+          <Image
             src="/assets/images/logo.svg"
             alt="logo"
             width={128}
@@ -14,7 +14,7 @@ const Footer = () => {
           />
         </Link>
 
-        <p>2023 Evently. All Rights reserved.</p>
+        <p>2024 UniVibe. All Rights reserved.</p>
       </div>
     </footer>
   )
