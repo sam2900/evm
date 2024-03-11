a4133d52a3129d1eb2f08ff48aadf83ac28ed847
Saksham Mathur <144251472+sam2900@users.noreply.github.com>
2024-03-11 08:57:22 +0530

    Update user.model.ts

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -1,12 +1,12 @@
 import { Schema, model, models } from "mongoose";
 
 const UserSchema = new Schema({
-  clerkId: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  username: { type: String, required: true, unique: true },
-  firstName: { type: String, required: true },
-  lastName: {type: String, required: true },
-  photo: { type: String, required: true },
+  clerkId: { type: String, require: true, unique: true },
+  email: { type: String, require: true, unique: true },
+  username: { type: String, require: true, unique: true },
+  firstName: { type: String, require: true },
+  lastName: {type: String, require: true },
+  photo: { type: String, require: true },
 })
 
 const User = models.User || model('User', UserSchema);
